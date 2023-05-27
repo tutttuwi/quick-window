af6d799c53be0fa51ade495f153a5fd795129de6
t-tsutsui <t.tsutsui9117@gmail.com>
2023-05-28 01:18:36 +0900

    [wip] popup window

--- src/background/i18n.ts
+++ src/background/i18n.ts
@@ -1,11 +1,11 @@
-// import en from '../../public/_locales/en/messages.json';
+import en from '../../public/_locales/en/messages.json';
 import ja from '../../public/_locales/ja/messages.json';
 
-const messages = { ja };
+const messages = { en, ja };
 type Language = keyof typeof messages;
-type MessageKey = keyof typeof ja;
+type MessageKey = keyof typeof en & keyof typeof ja;
 
 export const getMessage = (key: MessageKey) => {
   const lang = navigator.language.slice(0, 2) as Language;
-  return messages[Object.keys(messages).includes(lang) ? lang : 'ja'][key].message;
+  return messages[Object.keys(messages).includes(lang) ? lang : 'en'][key].message;
 };
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,4 +1,32 @@
 <script setup lang="ts">
+
+const tabList: Ref<Array<any>> = ref([]);
+const iframeUrl: Ref<string> = ref("");
+
 </script>
 
-<template></template>
+<template>
+  <div class="popup-container">
+    <div class="tabs-container">
+      <template v-for="tab of tabList">
+        <div class="tab-item">
+          <div>{{ tab.name }}</div>
+        </div>
+
+        <div class="tools-container">
+          <div class="iframe-reload">
+          </div>
+          <div class="iframe-url">
+            <input class="form-control" type="text" name="input-url" :value="tab.url">
+          </div>
+        </div>
+      </template>
+    </div>
+
+    <div class="iframe-body">
+      <iframe class="list-scroll" :src="iframeUrl" frameborder="0" style="border: 0" width="500" height="800"
+        scrolling="no"></iframe>
+    </div>
+
+  </div>
+</template>
